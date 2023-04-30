9ab72598c64360767d5de61c570223d26ceac23a
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-04-30 19:59:05 -0300

    Lista composta e análise de dados

--- Exercises/ex084.py
+++ Exercises/ex084.py
@@ -2,19 +2,33 @@
 
 temp = []
 pessoas = []
-cont = 0
+mai = men = 0
 
 while True:
     temp.append(str(input("Qual o seu nome? ")))
-    temp.append((input("Qual seu peso ")))
+    temp.append((input("Qual seu peso? ")))
+    if len(pessoas) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    
     pessoas.append(temp[:])
     temp.clear() # Limpar ou ficará duplicada
-
+    
     r = str(input("Deseja continuar? [S/N] ")).upper()
     if r.startswith("N"):
         break 
-    
-    
 
-print(pessoas)
 print(f"Foram cadastradas {len(pessoas)} pessoas.")
+print(f"O maior peso foi de {mai}Kg. Peso de ", end="")
+for p in pessoas:
+    if p[1] == mai:
+        print(f"[{p[0]}] ", end="")
+print(f"O menor peso foi {men}Kg. Peso de ", end="")
+for p in pessoas:
+    if p[1] == men:
+        print(f"[{p[0]}] ", end="")
+    
