94dd6009f83dbb9828ce230451dd4b88669f4209
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-02 01:04:17 -0300

    Palpites para a MegaSena

--- Exercises/ex088.py
+++ Exercises/ex088.py
@@ -1,26 +1,29 @@
 # Exercício 88
 
-from random import randint
+# from random import randint
 
-lista = []
-jogos = []
-num = randint(1, 60)
-quant = int(input("Quantos jogos você deseja fazer? "))
-total = 0
+# lista = []
+# jogos = []
+# num = randint(1, 60)
+# quant = int(input("Quantos jogos você deseja fazer? "))
+# total = 0
 
-while total < quant:
-    cont = 0
-    while True:
-        num = randint(1, 60)
-        if num not in lista:
-            lista.append(num)
-            cont += 1
-        if cont >= 6:
-            break
-    lista.sort()
-    jogos.append(lista[:])
-    lista.clear()
-    total += 1
-for i, l in enumerate(jogos):
-    print(f"Jogo {i}: {l:}")
+# while total < quant:
+#     cont = 0
+#     while True:
+#         num = randint(1, 60)
+#         if num not in lista:
+#             lista.append(num)
+#             cont += 1
+#         if cont >= 6:
+#             break
+#     lista.sort()
+#     jogos.append(lista[:])
+#     lista.clear()
+#     total += 1
+# for i, l in enumerate(jogos):
+#     print(f"Jogo {i}: {l:}")
             
+import random
+for x in range(int(input('Number of games: '))): 
+    print(f'Game {x+1}: {random.sample(range(1, 61), 6)}')
