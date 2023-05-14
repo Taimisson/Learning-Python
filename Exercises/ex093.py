51d3f574a2f795a5054419aa031672520c25aa4a
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-14 20:06:43 -0300

    Jogador de Futebol

---
+++ Exercises/ex093.py
@@ -0,0 +1,28 @@
+# Exercício 93
+
+jogador = {}
+gols = []
+
+jogador["Nome"] = str(input("Nome do Jogador: "))
+jogador["Partidas"] = int(input(f"Quantas partidas {jogador['Nome']} jogou? "))
+for c in range(jogador["Partidas"]):
+     gols.append(int(input(f"Quantos gols na partida {c + 1}? ")))
+    
+jogador["Gols"] = gols[:]    
+jogador["Total"] = sum(gols)
+
+print("-" * 30)
+
+print(jogador)
+
+print("-" * 30)
+
+for k, v in jogador.items():
+    print(f"{k} tem valor {v}.")
+
+print("-" * 30)
+
+print(f"O jogador {jogador['Nome']} jogou {jogador['Partidas']} partidas.")
+for i, v in enumerate(jogador["Gols"]):
+    print(f"    => Na partida {i + 1}, fez {v} gols.")
+print(f"Foi um total de {jogador['Total']} gols.")
