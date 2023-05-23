d4da21bcc7344b4e033b08afbbbd3dba618a2cfc
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-23 20:14:44 -0300

    Update

--- Exercises/ex095.py
+++ Exercises/ex095.py
@@ -1,14 +1,5 @@
 # Exercício 95
 
-# while True:
-#     resposta = int(input("1 or 2"))
-#     if resposta == 3:
-#         break
-#     else:
-#         print("Again")
-
-# 
-
 jogador = {}
 gols = []
 
