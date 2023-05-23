191d0fd00a855a4550cb2654d41c778c4f25450c
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-22 23:26:50 -0300

    Update

--- Exercises/ex095.py
+++ Exercises/ex095.py
@@ -9,5 +9,33 @@
 
 # 
 
-# print("Test")
+jogador = {}
+gols = []
 
+
+jogador["Nome"] = str(input("Nome do Jogador: "))
+jogador["Partidas"] = int(input(f"Quantas partidas {jogador['Nome']} jogou? "))
+for c in range(jogador["Partidas"]):
+     gols.append(int(input(f"Quantos gols na partida {c + 1}? ")))
+    
+jogador["Gols"] = gols[:]    
+jogador["Total"] = sum(gols)
+
+print("-" * 30)
+
+print(jogador)
+
+print("-" * 30)
+
+# Printa o dicionário
+for k, v in jogador.items():
+    print(f"{k} tem valor {v}.")
+
+print("-" * 30)
+
+print(f"O jogador {jogador['Nome']} jogou {jogador['Partidas']} partidas.")
+
+# Use enumerate para lISTAS
+for i, v in enumerate(jogador["Gols"]):  
+    print(f"    => Na partida {i + 1}, fez {v} gols.")
+print(f"Foi um total de {jogador['Total']} gols.")
