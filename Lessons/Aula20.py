945fdc90004b9e8b1e6f0b4af508d083d6d7132a
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-26 19:45:02 -0300

    Funções (Parte 1)

--- Lessons/Aula20.py
+++ Lessons/Aula20.py
@@ -1,8 +1,29 @@
 # Funções (Parte 1)
 
-def mostraLinha():
-    print("-"*30)
+# def mostraLinha():
+#     print("-"*30)
     
-mostraLinha()
-print("Teste")
-mostraLinha()
+# mostraLinha()
+# print("Teste")
+# mostraLinha()
+
+# -------
+
+# def mensagem(msg):
+#     print("-"*30)
+#     print(msg)
+#     print("-"*30)
+    
+# mensagem("SISTEMA DE ALUNOS")
+
+# -------
+
+def soma(a,b):
+    s = a + b
+    print(s)
+    
+soma(4,5)
+
+soma(8,9)
+
+soma(2,1)
