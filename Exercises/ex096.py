834e300d5d8fad18d166a315cfdfa06264393041
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-27 22:35:38 -0300

    Função que calcula área

--- Exercises/ex096.py
+++ Exercises/ex096.py
@@ -1,13 +1,16 @@
 # Exercício 96
 
 def area(largura, comprimento):
-    print("="*30)
-    print("CONSTROLE DE TERRENOS")
-    print("="*30)
+
     dimensao = largura * comprimento
     print(f"A área de um terreno {largura}x{comprimento} é de {dimensao}m²")
     
-
-l = float(input("Largura (m): "))
-m = float(input("Comprimento (m): "))
-area(l,m)
+    
+ # Programa Principal
+ 
+print("="*30)
+print("CONSTROLE DE TERRENOS")
+print("="*30)
+larg = float(input("Largura (m): "))
+comp = float(input("Comprimento (m): "))
+area(larg, comp)
