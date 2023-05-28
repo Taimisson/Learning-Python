4ca4ae138e1d1b319ca2691e197d0b5cfd0ae78f
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-28 02:55:11 -0300

    Função que descobre o maior

--- Exercises/ex099.py
+++ Exercises/ex099.py
@@ -1,1 +1,19 @@
 # Exercício 99
+
+def encontrarMaiorValor(*num):
+    if num:
+        print("-"*30)
+        print(f"Os valores digitados foram {num}")
+        maior = max(num)
+        print(f"O maior valor fornecido é {maior}")
+    else: 
+        print("Nenhum valor fornecido")
+    print("-"*30)
+    
+
+
+encontrarMaiorValor(2, 9, 4, 5, 7, 1)
+encontrarMaiorValor(4, 7, 0)
+encontrarMaiorValor(1, 2)
+encontrarMaiorValor(6)
+encontrarMaiorValor()
