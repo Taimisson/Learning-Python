1d2f3f22c33edf30524d3d7aefe4f7f73d358981
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-27 23:40:34 -0300

    Contador

---
+++ Exercises/ex098.py
@@ -0,0 +1,28 @@
+# Exercício 98
+
+def contador(i, f, p):
+    print(f"Contagem de {i} até {f} de {p} em {p}")
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f"{cont} ", end="")
+            cont += p
+        print("FIM")
+    else:
+        cont = 1
+        while cont >= f:
+            print(f"{cont} ", end="")
+            cont -= p
+        print("FIM!")
+        
+# Programa Principal
+contador(1, 10, 1)
+contador(10, 0, 2)
+print("Agora é a sua vez!")
+
+contador(
+    int(input("Início: ")), 
+    int(input("Fim: ")),
+    int(input("Passo: "))
+ )
