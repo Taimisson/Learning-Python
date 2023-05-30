c9f0f77700c1672e94c713991128760709dbdff6
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-05-30 00:02:34 -0300

    Funções para votação

--- Exercises/ex101.py
+++ Exercises/ex101.py
@@ -1,1 +1,17 @@
 # Exercício 101
+
+def voto(ano):
+    
+    from datetime import date
+    anoAtual = date.today().year
+    idade = anoAtual - ano
+    
+    if idade < 16:
+        return f"Você tem {idade}: NÃO VOTA"
+    elif 16 <= idade < 18 or idade > 65:
+        return f"Você tem {idade} anos: VOTO OPCIONAL"
+    else:
+        return f"Você tem {idade} anos: VOTO OBRIGATÓRIO"
+    
+nasc = int(input("Em que ano você nasceu? "))
+print(voto(nasc))
