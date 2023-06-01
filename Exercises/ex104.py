01cf0494adbf4692d2257300c0314f0c006c30ef
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-01 00:41:50 -0300

    Validando entrada de dados em Python

--- Exercises/ex104.py
+++ Exercises/ex104.py
@@ -1,1 +1,14 @@
 # Exercício 104
+
+def leiaInt(msg):
+    
+    while True:
+        num = input(msg)
+        if num.isnumeric():
+            return num
+            break
+        else:
+            print("ERRO! Você não digitou um número, tente novamente!")
+
+n = leiaInt("Digite um número: ")
+print(f"Você acabou de digitar o número {n}")
