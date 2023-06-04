4e904e5eedbfdc9d8604724bdd8e9661f0722443
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-04 19:10:45 -0300

    Módulos e Pacotes

--- Lessons/Aula22.py
+++ Lessons/Aula22.py
@@ -1,21 +1,11 @@
 # Aula 22
 # Módulos e Pacotes
 
-def fatorial(n):
-    f = 1
-    for c in range(1, n+1):
-        f *= c
-    return f
-
-def dobro(n):
-    return n * 2;
-
-def triplo(n):
-    return n * 3;
-
+# from uteis import fatorial, dobro
+import uteis
 
 # Programa principal
 num = int(input("Digite um valor: "))
-fat = fatorial(num)
+fat = uteis.fatorial(num)
 print(f"O fatorial de {num} é {fat}")
-print(f"O dobro de {num} é {dobro(num)}")
+print(f"O dobro de {num} é {uteis.dobro(num)}")
