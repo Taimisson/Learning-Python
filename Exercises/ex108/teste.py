2630c0bac87e4ae6bb9077e6fe4d40501e080c3c
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-04 20:50:55 -0300

    Formatando Moedas em Python

---
+++ Exercises/ex108/teste.py
@@ -0,0 +1,10 @@
+# Exercício 107
+
+import moeda
+
+# Programa Principal
+preço = float(input("Digite o preço: R$"))
+print(f"A metade de {moeda.moeda(preço)} é {moeda.moeda(moeda.metade(preço))}")
+print(f"O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço)}")
+print(f"Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preço, 10))}")
+ 
