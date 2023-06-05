b6ff520ef2afd9ff5ceed4547f55fbdfca528873
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-05 19:22:43 -0300

    Moedas em Python

--- Exercises/ex109/teste.py
+++ Exercises/ex109/teste.py
@@ -4,8 +4,8 @@ import moeda
 
 # Programa Principal
 preço = float(input("Digite o preço: R$"))
-print(f"A metade de {moeda.moeda(preço)} é {moeda.moeda(moeda.metade(preço))}")
-print(f"O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço)}")
-print(f"Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preço, 10))}")
-print(f"Reduzindo 13%, temos {moeda.diminuir(preço, 13)}")
+print(f"A metade de {moeda.moeda(preço)} é {moeda.metade(preço, True)}")
+print(f"O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço, True)}")
+print(f"Aumentando 10%, temos {moeda.aumentar(preço, 10, True)}")
+print(f"Reduzindo 13%, temos {moeda.diminuir(preço, 13, True)}")
  
