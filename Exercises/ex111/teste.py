3a25ced3e7147559fa04f6f67156971f614c6c53
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-05 23:06:16 -0300

    Entrada de dados monetários

--- Exercises/ex111/teste.py
+++
@@ -1,7 +0,0 @@
-# Exercício 111
-
-from ex111.utilidadesCV import moeda
-
-# Programa Principal
-p = float(input("Digite o preço: R$"))
-moeda.resumo(p, 20, 12)
---
+++ Exercises/ex111/utilidadesCV/eex111.py
@@ -0,0 +1,7 @@
+# Exercício 111
+
+import moeda, dado
+
+# Programa Principal
+p = dado.leiaDinheiro("Digite o preço: R$")
+moeda.resumo(p, 20, 12)
