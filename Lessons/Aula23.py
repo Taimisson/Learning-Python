b031d4097d754cf757308f614cb0c58875d484cd
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-09 00:09:59 -0300

    Tratamento de Erros e Exceções

--- Lessons/Aula23.py
+++ Lessons/Aula23.py
@@ -1,5 +1,10 @@
 # Aula 23
 # Tratamento de erros
 
-n = int(input("Número: "))
-print(n)
+# n = int(input("Número: ")) # "Oito" não aceita
+# print(n)
+
+a = int(input("Numerador: "))
+b = int(input("Denominador: "))
+r = a / b
+print(f"O resultado é {r}")
