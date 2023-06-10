15074b5da2e91cba07eb60cb5005fa29fe0c8787
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-06-09 21:00:22 -0300

    Tratamento de Erros em Python

--- Lessons/Aula23.py
+++ Lessons/Aula23.py
@@ -8,9 +8,17 @@ try:
     a = int(input("Numerador: "))
     b = int(input("Denominador: "))
     r = a / b
-except:
-    print("Infelizmente tivemos um problema :(")
+except (ValueError, TypeError):
+    print(f"Tivemos um problema com os tipos de dados que você digitou.")
+except ZeroDivisionError:
+    print("Não é possível dividir um número por zero!")
+except KeyboardInterrupt:
+    print("O usuário preferiu não informar os dados!")
+except Exception as erro:
+    print(f"O erro encontrado foi {erro.__cause__}")
 else:
     print(f"O resultado é {r:.1f}")
 finally:
     print("Volte sempre! Muito obrigado!")
+    
+    
