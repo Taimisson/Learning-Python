73147ca07817f14308cbcdb07f3440a5e7cd7184
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-08-14 20:57:01 -0300

    Add files via upload

---
+++ Algoritmos e Programação/Exercícios/ex005.py
@@ -0,0 +1,13 @@
+# Exercício 5
+
+num1 = int(input("Digite o primeiro número: "));
+num2 = int(input("Digite o segundo número: "));
+num3 = int(input("Digite o terceiro número: "));
+num4 = int(input("Digite o quarto número: "));
+num5 = int(input("Digite o quinto número: "));
+
+soma = num1 + num2 + num3 + num4 + num5
+produto = num1 * num2 * num3 * num4 * num5 
+
+print("SOMA TOTAL:", soma);
+print("PRODUTO:", produto);
