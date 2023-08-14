58347dd6de1a91d9582afe91b1b0d9f17bf5b7fc
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-08-14 20:57:20 -0300

    Add files via upload

---
+++ Algoritmos e Programação/Exercícios/ex006.py
@@ -0,0 +1,14 @@
+A = int(input("Digite o primeiro número: "));
+B = int(input("Digite o segundo número: "));
+C = int(input("Digite o terceiro número: "));
+D = int(input("Digite o quarto número: "));
+E = int(input("Digite o quinto número: "));
+
+areaTriangulo = (B * C) / 2
+print(f"Área do Triângulo: {areaTriangulo}");
+
+perimetroRetangulo = A + B + C + D
+print(f"Perímetro do Retângulo: {perimetroRetangulo}");
+
+areaCirculo = 3.14 * E**2 
+print(f"Área do Círculo: {areaCirculo}");
