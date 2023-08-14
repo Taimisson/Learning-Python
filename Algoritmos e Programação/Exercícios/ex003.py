ac44d75af59f3c6098b31ca765f488ee0430ccd5
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-08-14 20:56:07 -0300

    Add files via upload

---
+++ Algoritmos e Programação/Exercícios/ex003.py
@@ -0,0 +1,9 @@
+# Exercício 3
+
+nome = str(input("Digite o seu nome: "));
+altura = input("Digite a sua altura: ");
+
+print("Nome: " + nome);
+print("Altura: " + altura);
+print("Tenha um bom dia!");
+
