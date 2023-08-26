018a7880ce3b8668c0a98068571489b2f11ed533
Taimisson de Carvalho <100889845+Taimisson@users.noreply.github.com>
2023-08-26 18:25:32 -0300

    Soma dos valores

---
+++ Algoritmos e Programação - Unisinos/Exercícios/ex036.py
@@ -0,0 +1,9 @@
+# Soma dos valores
+
+soma = 0
+i = 0
+while i < 10:
+    valor = int(input("Digite um valor inteiro: "))
+    soma += valor
+    i += 1
+print(f"Soma dos valores digitados: {soma}")
